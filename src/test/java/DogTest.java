76a16744b225e04f2f2baedecc84e9b647346292
FFucks <fabio_geiss@hotmail.com>
2025-05-04 23:03:07 -0300

    Added initial tests

---
+++ src/test/java/DogTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DogTest {
+
+    @Test
+    public void testDogFields() {
+        Object dog = new Dog();
+        Field[] fields = dog.getClass().getDeclaredFields();
+
+        List<String> actualFieldNames = getFieldNames(fields);
+
+        assertTrue(Arrays.asList("name", "breed")
+                .containsAll(actualFieldNames));
+    }
+
+    private static List<String> getFieldNames(Field[] fields) {
+        List<String> fieldNames = new ArrayList<>();
+        for (Field field : fields)
+            fieldNames.add(field.getName());
+        return fieldNames;
+    }
+}
